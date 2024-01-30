b04a6b81eafac8c0aca92c7aa04cf616fc2f2fb0
DorinVieru <dorino.vieru@gmail.com>
2024-01-30 17:18:34 +0100

    [UP] Creazione della classe movie, crezione delle istanze e assegnazione dei valori agli attributi

--- index.php
+++ index.php
@@ -1,5 +1,49 @@
 <!-- SEZIONE PHP LOGICA -->
 <?php
+// DEFICNIZIONE DELLA CLASSE MOVIE
+class Movie
+{
+    public $name;
+    public $genre;
+    public $princ_actor;
+    public $regista;
+    public $year;
+    public $duration;
+    public $original_language;
+}
+
+// CREO ISTANZA PER LA CLASSE MOVIE
+$movie_1 = new Movie();
+$movie_2 = new Movie();
+$movie_3 = new Movie();
+
+// ASSEGNO DEI VALORI AGLI ATTRIBUTI DELL'ISTANZA DI MOVIE 1
+$movie_1->name = "V per Vendetta";
+$movie_1->genre = "Azione, Drammatico, Thriller";
+$movie_1->princ_actor = "Hugo Weaving";
+$movie_1->regista = "James McTeigue";
+$movie_1->year = 2005;
+$movie_1->duration = "132 min";
+$movie_1->original_language = "Inglese";
+
+// ASSEGNO DEI VALORI AGLI ATTRIBUTI DELL'ISTANZA DI MOVIE 2
+$movie_2->name = "La battaglia di Hacksaw Ridge";
+$movie_2->genre = "Biografico, Drammatico, Storico, Guerra";
+$movie_2->princ_actor = "Andrew Garfield";
+$movie_2->regista = "Mel Gibson";
+$movie_2->year = 2016;
+$movie_2->duration = "131 min";
+$movie_2->original_language = "Inglese";
+
+// ASSEGNO DEI VALORI AGLI ATTRIBUTI DELL'ISTANZA DI MOVIE 3
+$movie_3->name = "The Place";
+$movie_3->genre = "Drammatico, Noir, Fantastico";
+$movie_3->princ_actor = "Valerio Mastandrea";
+$movie_3->regista = "Paolo Genovese";
+$movie_3->year = "2017";
+$movie_3->duration = "105 min";
+$movie_3->original_language = "Italiano";
+
 
 ?>
 
@@ -18,11 +62,41 @@
 <body>
     <main class="container my-5">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 pb-4">
                 <h1 class="text-center text-primary">The Best Movie Ever You Seen</h1>
             </div>
-            <div class="col-12">
-                
+            <div class="col-4">
+                <ul class="list-group">
+                    <li class="list-group-item fw-bold">Titolo: <?php echo $movie_1->name ?></li>
+                    <li class="list-group-item">Genere: <?php echo $movie_1->genre ?></li>
+                    <li class="list-group-item">Anno di uscita: <?php echo $movie_1->year ?></li>
+                    <li class="list-group-item">Lingua originale: <?php echo $movie_1->original_language ?></li>
+                    <li class="list-group-item">Durata: <?php echo $movie_1->duration ?></li>
+                    <li class="list-group-item">Regista: <?php echo $movie_1->regista ?></li>
+                    <li class="list-group-item">Attore principale: <?php echo $movie_1->princ_actor ?></li>
+                </ul>
+            </div>
+            <div class="col-4">
+                <ul class="list-group">
+                    <li class="list-group-item fw-bold">Titolo: <?php echo $movie_2->name ?></li>
+                    <li class="list-group-item">Genere: <?php echo $movie_2->genre ?></li>
+                    <li class="list-group-item">Anno di uscita: <?php echo $movie_2->year ?></li>
+                    <li class="list-group-item">Lingua originale: <?php echo $movie_2->original_language ?></li>
+                    <li class="list-group-item">Durata: <?php echo $movie_2->duration ?></li>
+                    <li class="list-group-item">Regista: <?php echo $movie_2->regista ?></li>
+                    <li class="list-group-item">Attore principale: <?php echo $movie_2->princ_actor ?></li>
+                </ul>
+            </div>
+            <div class="col-4">
+                <ul class="list-group">
+                    <li class="list-group-item fw-bold">Titolo: <?php echo $movie_3->name ?></li>
+                    <li class="list-group-item">Genere: <?php echo $movie_3->genre ?></li>
+                    <li class="list-group-item">Anno di uscita: <?php echo $movie_3->year ?></li>
+                    <li class="list-group-item">Lingua originale: <?php echo $movie_3->original_language ?></li>
+                    <li class="list-group-item">Durata: <?php echo $movie_3->duration ?></li>
+                    <li class="list-group-item">Regista: <?php echo $movie_3->regista ?></li>
+                    <li class="list-group-item">Attore principale: <?php echo $movie_3->princ_actor ?></li>
+                </ul>
             </div>
         </div>
     </main>
