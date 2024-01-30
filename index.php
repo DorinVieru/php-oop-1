ab99b7fed84e69a3456aa5480f01583201c210ad
DorinVieru <dorino.vieru@gmail.com>
2024-01-30 13:40:43 +0100

    [UP] Create a basic structure

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- SEZIONE PHP LOGICA -->
+<?php
+
+?>
+
+<!-- SEZIONE HTML -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Movie</title>
+</head>
+
+<body>
+    <main class="container my-5">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-primary">The Best Movie Ever You Seen</h1>
+            </div>
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
