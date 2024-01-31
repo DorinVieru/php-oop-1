d99f01e453ca76e95a6beede211f68fad240c270
DorinVieru <dorino.vieru@gmail.com>
2024-01-31 10:03:54 +0100

    [BONUS] Improve code & print genre array on html; complete exercise

--- index.php
+++ index.php
@@ -31,39 +31,16 @@
         {
             return $this->name . " <span class='badge bg-warning'>Da rivedere</span>";
         }
+
+        public function forgenre(){
+            return implode(',', $this->genre);
+        }
     }
 
     // CREO ISTANZA PER LA CLASSE MOVIE
     $movie_1 = new Movie("V per Vendetta", ["Azione, Drammatico, Thriller"], "James McTeigue", 2005, "132 min");
     $movie_2 = new Movie("La battaglia di Hacksaw Ridge", ["Biografico, Drammatico, Storico, Guerra"], "Mel Gibson", 2016, "131 min");
     $movie_3 = new Movie("The Place", ["Drammatico, Noir, Fantastico"], "Paolo Genovese", 2017, "105 min");
-
-    // ASSEGNO DEI VALORI AGLI ATTRIBUTI DELL'ISTANZA DI MOVIE 1
-    $movie_1->name = "V per Vendetta";
-    $movie_1->genre = "Azione, Drammatico, Thriller";
-    $movie_1->princ_actor = "Hugo Weaving";
-    $movie_1->regista = "James McTeigue";
-    $movie_1->year = 2005;
-    $movie_1->duration = "132 min";
-    $movie_1->original_language = "Inglese";
-
-    // ASSEGNO DEI VALORI AGLI ATTRIBUTI DELL'ISTANZA DI MOVIE 2
-    $movie_2->name = "La battaglia di Hacksaw Ridge";
-    $movie_2->genre = "Biografico, Drammatico, Storico, Guerra";
-    $movie_2->princ_actor = "Andrew Garfield";
-    $movie_2->regista = "Mel Gibson";
-    $movie_2->year = 2016;
-    $movie_2->duration = "131 min";
-    $movie_2->original_language = "Inglese";
-
-    // ASSEGNO DEI VALORI AGLI ATTRIBUTI DELL'ISTANZA DI MOVIE 3
-    $movie_3->name = "The Place";
-    $movie_3->genre = "Drammatico, Noir, Fantastico";
-    $movie_3->princ_actor = "Valerio Mastandrea";
-    $movie_3->regista = "Paolo Genovese";
-    $movie_3->year = "2017";
-    $movie_3->duration = "105 min";
-    $movie_3->original_language = "Italiano";
 
 ?>
 
@@ -89,7 +66,7 @@
             <div class="col-4">
                 <ul class="list-group">
                     <li class="list-group-item fw-bold">Titolo: <?php echo $movie_1->filmConsigliato(); ?></li>
-                    <li class="list-group-item">Genere: <?php echo $movie_1->genre ?></li>
+                    <li class="list-group-item">Genere: <?php echo $movie_1->forgenre(); ?></li>
                     <li class="list-group-item">Anno di uscita: <?php echo $movie_1->year ?></li>
                     <li class="list-group-item">Lingua originale: <?php echo $movie_1->original_language ?></li>
                     <li class="list-group-item">Durata: <?php echo $movie_1->duration ?></li>
@@ -101,7 +78,7 @@
             <div class="col-4">
                 <ul class="list-group">
                     <li class="list-group-item fw-bold">Titolo: <?php echo $movie_2->name ?></li>
-                    <li class="list-group-item">Genere: <?php echo $movie_2->genre ?></li>
+                    <li class="list-group-item">Genere: <?php echo $movie_2->forgenre(); ?></li>
                     <li class="list-group-item">Anno di uscita: <?php echo $movie_2->year ?></li>
                     <li class="list-group-item">Lingua originale: <?php echo $movie_2->original_language ?></li>
                     <li class="list-group-item">Durata: <?php echo $movie_2->duration ?></li>
@@ -113,7 +90,7 @@
             <div class="col-4">
                 <ul class="list-group">
                     <li class="list-group-item fw-bold">Titolo: <?php echo $movie_3->filmRivedere(); ?></li>
-                    <li class="list-group-item">Genere: <?php echo $movie_3->genre ?></li>
+                    <li class="list-group-item">Genere: <?php echo $movie_3->forgenre(); ?></li>
                     <li class="list-group-item">Anno di uscita: <?php echo $movie_3->year ?></li>
                     <li class="list-group-item">Lingua originale: <?php echo $movie_3->original_language ?></li>
                     <li class="list-group-item">Durata: <?php echo $movie_3->duration ?></li>
